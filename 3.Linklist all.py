c886297131412282895b4e2be1a8f925137a199c
Pallab <pallab.basak.924@gmail.com>
2026-04-24 23:30:54 +0530

    all details about linklist

--- 3.Linklist all.py
+++ 3.Linklist all.py
@@ -3,3 +3,8 @@ class Node:
         self.data = value
         self.next = None
 
+class LinkedList:
+    def __init__(self,value):
+        #empty link list
+        self.head = None
+        self.n = 0
