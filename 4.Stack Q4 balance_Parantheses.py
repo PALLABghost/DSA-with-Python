1ff57bcf0695ca03438ddcc6f56eb756b20ee996
Pallab <pallab.basak.924@gmail.com>
2026-05-07 23:45:54 +0530

    balance backets in any equation

---
+++ 4.Stack Q4 balance_Parantheses.py
@@ -0,0 +1,16 @@
+
+def is_balanced(s: str) -> bool:        # “This function takes a string as input and returns a boolean result.
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in mapping.values():  # opening brackets
+            stack.append(char)
+        elif char in mapping:  # closing brackets       checking for key in dictionary
+            if not stack or stack[-1] != mapping[char]:#If the stack is empty OR the top of the stack doesn’t match the closing bracket, then return False.
+                return False
+            stack.pop()
+
+    return not stack
+
+print(is_balanced("{(a+b)}"))
