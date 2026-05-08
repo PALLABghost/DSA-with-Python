597c77c374af356e217cb25be73470d0d6523285
Pallab <pallab.basak.924@gmail.com>
2026-05-08 07:36:28 +0530

    balance parenthesis in any equation return Bool

--- 4.Stack Q4 balance_Parantheses.py
+++ 4.Stack Q4 balance_Parantheses.py
@@ -7,10 +7,10 @@ def is_balanced(s: str) -> bool:        # “This function takes a string as inp
         if char in mapping.values():  # opening brackets
             stack.append(char)
         elif char in mapping:  # closing brackets       checking for key in dictionary
-            if not stack or stack[-1] != mapping[char]:#If the stack is empty OR the top of the stack doesn’t match the closing bracket, then return False.
+            if (not stack) or stack[-1] != mapping[char]:#If the stack is empty OR the top of the stack doesn’t match the closing bracket, then return False.
                 return False
             stack.pop()
 
-    return not stack
+    return not stack        #check if stack is empty or not , if balance then stack should be empty (not stack) == True
 
-print(is_balanced("{(a+b)}"))
+print(is_balanced("{(a+b))(a-b)}"))
